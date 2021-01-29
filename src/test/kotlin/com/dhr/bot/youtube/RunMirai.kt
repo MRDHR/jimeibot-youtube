5316fa348efeb45a948bbcafad8c0b6523257658
denghaoran <864245039@qq.com>
2021-01-29 08:08:04 +0800

    提交油管的代码

--- src/test/kotlin/com/dhr/bot/youtube/RunMirai.kt
+++ src/test/kotlin/com/dhr/bot/youtube/RunMirai.kt
@@ -7,19 +7,19 @@ import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.load
 import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
 
 suspend fun main() {
-    //HTTP 代理，只能代理 HTTP 请求
-    System.setProperty("http.proxyHost", "127.0.0.1")
-    System.setProperty("http.proxyPort", "7890")
-    // HTTPS 代理，只能代理 HTTPS 请求
-    System.setProperty("https.proxyHost", "127.0.0.1")
-    System.setProperty("https.proxyPort", "7890")
+//    //HTTP 代理，只能代理 HTTP 请求
+//    System.setProperty("http.proxyHost", "127.0.0.1")
+//    System.setProperty("http.proxyPort", "7890")
+//    // HTTPS 代理，只能代理 HTTPS 请求
+//    System.setProperty("https.proxyHost", "127.0.0.1")
+//    System.setProperty("https.proxyPort", "7890")
 
     MiraiConsoleTerminalLoader.startAsDaemon()
 
     YoutubePlugin().load()
     YoutubePlugin().enable()
 
-    val bot = MiraiConsole.addBot(386165664, "Dhr13532201835") {
+    val bot = MiraiConsole.addBot(12345678, "") {
         fileBasedDeviceInfo()
     }.alsoLogin()
 
